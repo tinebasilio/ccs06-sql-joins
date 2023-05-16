265386ed6e1f42c9e440502177d1b8104e2b9385
Kristine <kristinebasilio28@gmail.com>
2023-05-17 01:48:57 +0800

    Added department-based employee listing functionality

--- src/Employee.php
+++ src/Employee.php
@@ -6,22 +6,34 @@ use Exception;
 
 class Employee
 {
-    public static function list()
+    public static function listByDepartment($dept_no)
     {
         global $conn;
 
         try {
-            $sql = "SELECT * FROM employees LIMIT 5";
+            $sql = "SELECT t.title,
+                           CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+                           e.emp_no, e.birth_date, TIMESTAMPDIFF(YEAR, e.birth_date, CURDATE()) AS employee_age,
+                           e.gender, e.hire_date, FORMAT(s.salary, 'C') AS salary
+                   FROM employees e
+                   INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+                   INNER JOIN titles t ON e.emp_no = t.emp_no
+                   INNER JOIN salaries s ON e.emp_no = s.emp_no
+                   WHERE de.dept_no = :dept_no
+                   AND t.to_date = '9999-01-01'
+                   AND s.to_date = '9999-01-01'
+                   LIMIT 10";
 
             $statement = $conn->prepare($sql);
             $statement->execute();
-            $records = [];
+
+            $employees = [];
 
             while ($row = $statement->fetch()) {
-                array_push($records, $row);
+                array_push($employees, $row);
             }
 
-            return $records;
+            return $employees;
         } catch (Exception $e) {
             error_log($e->getMessage());
         }
