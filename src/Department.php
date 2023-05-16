7119be060aceadce8c2ae23275241ac3a1cf524b
Kristine <kristinebasilio28@gmail.com>
2023-05-17 01:45:53 +0800

    Add department listing functionality

--- src/Department.php
+++ src/Department.php
@@ -11,7 +11,14 @@ class Department
         global $conn;
 
         try {
-            $sql = "SELECT * FROM departments LIMIT 5";
+            $sql = "SELECT d.dept_no, d.dept_name, 
+                           CONCAT(e.first_name, ' ', e.last_name) AS manager_name, dm.from_date, dm.to_date, 
+                           TIMESTAMPDIFF(YEAR, dm.from_date, dm.to_date) AS num_years 
+                    FROM departments d 
+                    INNER JOIN dept_manager dm ON d.dept_no = dm.dept_no 
+                    INNER JOIN employees e ON dm.emp_no = e.emp_no 
+                    ORDER BY dm.from_date 
+                    LIMIT 10";
 
             $statement = $conn->prepare($sql);
             $statement->execute();
