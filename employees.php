aaff59ce0370293d673ca311ca56089da215bd05
Kristine Angeli Basilio <92997362+tinebasilio@users.noreply.github.com>
2023-05-17 02:26:21 +0800

    Updated code

--- employees.php
+++ employees.php
@@ -10,56 +10,100 @@ if (isset($_GET['dept_no'])) {
 }
 ?>
 
-<?php
-   // Retrieve the list of employees in the department
-   $emps = Employee::listByDepartment($dept_no);
+<!DOCTYPE html>
+<html>
+<head>
+      <style>
+      body {
+         font-family: Arial, sans-serif;
+         margin: 0;
+         padding: 20px;
+      }
 
-   // Display the table headers
-   echo "<table>";
-   echo "<tr><th>Employee Title</th><th>Employee Name</th><th>Birthday</th><th>Age</th><th>Gender</th><th>Hire Date</th><th>Latest Salary</th><th>Link</th></tr>";
+      table {
+         width: 100%;
+         border-collapse: collapse;
+      }
 
-   // Query to retrieve the department name and manager name
-   $sql = "SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name
-            FROM departments d
-            JOIN dept_manager dm 
-               ON d.dept_no = dm.dept_no
-            JOIN employees e 
-               ON dm.emp_no = e.emp_no
-            WHERE d.dept_no = :dept_no
-            LIMIT 1";
-   $statement = $conn->prepare($sql);
-   $statement->bindValue(':dept_no', $dept_no);
-   $statement->execute();
-   $display = $statement->fetch();
+      th, td {
+         padding: 10px;
+         text-align: left;
+         border-bottom: 1px solid #ddd;
+      }
 
-   // Display the department name and manager name
-   echo "<h1>{$display['dept_name']}</h1>";
-   echo "<p>Department Manager: {$display['manager_name']}</p>";
+      th {
+         background-color: #f5f5f5;
+         font-weight: bold;
+      }
 
-   // Loop through each employee in the department to retrieve the latest salary information of the employee
-   foreach ($emps as $emp) {
-      $sql = "SELECT FORMAT(s.salary, 'C') AS salary, s.from_date, s.to_date
-              FROM salaries s
-              WHERE emp_no = :emp_no
-              ORDER BY to_date DESC
-              LIMIT 1";
+      tr:nth-child(even) {
+         background-color: #f9f9f9;
+      }
 
+      tr:hover {
+         background-color: #f5f5f5;
+      }
+
+      a {
+         text-decoration: none;
+         color: #007bff;
+      }
+   </style>
+</head>
+<body>
+   
+   <?php
+      // Retrieve the list of employees in the department
+      $emps = Employee::listByDepartment($dept_no);
+
+      // Display the table headers
+      echo "<table>";
+      echo "<tr><th>Employee Title</th><th>Employee Name</th><th>Birthday</th><th>Age</th><th>Gender</th><th>Hire Date</th><th>Latest Salary</th><th>Link</th></tr>";
+
+      // Query to retrieve the department name and manager name
+      $sql = "SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name
+               FROM departments d
+               JOIN dept_manager dm 
+                  ON d.dept_no = dm.dept_no
+               JOIN employees e 
+                  ON dm.emp_no = e.emp_no
+               WHERE d.dept_no = :dept_no
+               LIMIT 1";
       $statement = $conn->prepare($sql);
-      $statement->bindValue(':emp_no', $emp['emp_no']);
+      $statement->bindValue(':dept_no', $dept_no);
       $statement->execute();
-      
-      // Display employee information in each table row
-      echo "<tr>";
-      echo "<td>{$emp['title']}</td>";
-      echo "<td>{$emp['employee_name']}</td>";
-      echo "<td>{$emp['birth_date']}</td>";
-      echo "<td>{$emp['employee_age']}</td>";
-      echo "<td>{$emp['gender']}</td>";
-      echo "<td>{$emp['hire_date']}</td>";
-      echo "<td>\${$emp['salary']}</td>";
-      echo "<td><a href=\"/salary-history.php?emp_no={$emp['emp_no']}\">View Salary History</a></td>";
-      echo "</tr>";
-   }
+      $display = $statement->fetch();
 
-   echo "</table>";
-?>
+      // Display the department name and manager name
+      echo "<h1>{$display['dept_name']}</h1>";
+      echo "<p>Department Manager: {$display['manager_name']}</p>";
+
+      // Loop through each employee in the department to retrieve the latest salary information of the employee
+      foreach ($emps as $emp) {
+         $sql = "SELECT FORMAT(s.salary, 'C') AS salary, s.from_date, s.to_date
+                 FROM salaries s
+                 WHERE emp_no = :emp_no
+                 ORDER BY to_date DESC
+                 LIMIT 1";
+
+         $statement = $conn->prepare($sql);
+         $statement->bindValue(':emp_no', $emp['emp_no']);
+         $statement->execute();
+
+         // Display employee information in each table row
+         echo "<tr>";
+         echo "<td>{$emp['title']}</td>";
+         echo "<td>{$emp['employee_name']}</td>";
+         echo "<td>{$emp['birth_date']}</td>";
+         echo "<td>{$emp['employee_age']}</td>";
+         echo "<td>{$emp['gender']}</td>";
+         echo "<td>{$emp['hire_date']}</td>";
+         echo "<td>\${$emp['salary']}</td>";
+         echo "<td><a href=\"/salary-history.php?emp_no={$emp['emp_no']}\">View Salary History</a></td>";
+         echo "</tr>";
+      }
+
+      echo "</table>";
+   ?>
+</body>
+</html>
