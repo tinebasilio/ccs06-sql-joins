0e2b524c1dcff949dd38eae8576a39c0bf53f9e1
Kristine Angeli Basilio <92997362+tinebasilio@users.noreply.github.com>
2023-05-18 01:14:39 +0800

    Updated code

--- salary-history.php
+++ salary-history.php
@@ -52,24 +52,24 @@ if (isset($_GET['emp_no'])) {
 <?php
    // Query to retrieve the employee's salary history and basic information
    $sql = "SELECT
-				CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
-				e.birth_date, e.gender, e.hire_date, FORMAT(s.salary, 'C') AS salary,
-				s.from_date,s.to_date, MAX(t.title) AS title, MAX(d.dept_name) AS dept_name
-			FROM employees AS e
-			JOIN salaries AS s ON e.emp_no = s.emp_no
-			JOIN titles AS t ON e.emp_no = t.emp_no
-			JOIN dept_emp AS de ON e.emp_no = de.emp_no
-			JOIN departments AS d ON de.dept_no = d.dept_no
-			WHERE e.emp_no = :emp_no
-			GROUP BY
-			  employee_name,
-			  birth_date,
-			  gender,
-			  hire_date,
-			  salary,
-			  from_date,
-			  to_date
-			ORDER BY from_date DESC";
+		CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+		e.birth_date, e.gender, e.hire_date, FORMAT(s.salary, 'C') AS salary,
+		s.from_date,s.to_date, MAX(t.title) AS title, MAX(d.dept_name) AS dept_name
+	   FROM employees AS e
+	   JOIN salaries AS s ON e.emp_no = s.emp_no
+	   JOIN titles AS t ON e.emp_no = t.emp_no
+	   JOIN dept_emp AS de ON e.emp_no = de.emp_no
+	   JOIN departments AS d ON de.dept_no = d.dept_no
+	   WHERE e.emp_no = :emp_no
+     	   GROUP BY
+		employee_name,
+		birth_date,
+		gender,
+		hire_date,
+		salary,
+		from_date,
+		to_date
+	    ORDER BY from_date DESC";
 
    $statement = $conn->prepare($sql);
    $statement->bindValue(':emp_no', $emp_no);
